f35734eea00b22d04f3f77c41898a8ccf55a7447
Kaitlyn Parkhurst <symkat@symkat.com>
2022-02-13 14:08:48 -0800

    Fix up the CGI process.

--- CGI/lib/MarkdownSite/CGI.pm
+++ CGI/lib/MarkdownSite/CGI.pm
@@ -9,6 +9,7 @@ use Cache::Memcached::Fast;
 sub startup ($self) {
 
     my $domain = $ENV{HTTP_HOST};
+    my $r_path = $ENV{PATH_INFO};
 
     # Load config from the given domain url...
     $self->plugin('NotYAMLConfig', { file => "/var/www/$domain/config.yml" });
@@ -19,50 +20,39 @@ sub startup ($self) {
         namespace => "$domain/",
     });
 
-    $self->log->info( "Trying to load routes from memcached." );
-    my %route_info = %{ $mem->get( 'routes' ) || {} };
+    $self->log->info( "Processing request for ${domain}${r_path}" );
 
-    if ( ! %route_info ) {
-        $self->log->info( "Building routes and storing them in memcached." );
+    # Load Routes For Markdown Pages
+    my %route_info = %{ $mem->get( 'routes' ) || {} };
+    if (! %route_info ) {
         foreach my $file ( Mojo::File->new("/var/www/$domain/pages")->list_tree->each  ) {
+            if ( $file->extname ne 'md' ) {
+                $self->log->info( "Skipping non-markdown file " . $file->to_string );
+                next;
+            }
+
             my $md = Markdown::Compiler->new( source => $file->slurp );
 
-            # Now we figure out the slug for this page:
-
             # 1. If we have the key 'web.path' we will use that
-            # 2. From the file, for each config up, does web.path.format exist?
-            #       YES: Use this format to create slugs from the path name
-            #       NO : Continue
-            # 3. Finally, use the filename itself, switching md -> html
-            if ( $md->metadata->{'web.path'} ) {
+            # 2. Otherwise map the file -- without domain/pages -- and change to md -> html
+            if ( $md->metadata && $md->metadata->{'web.path'} ) {
                 $route_info{$md->metadata->{'web.path'}} = $file->path;
                 next;
+            } else {
+                my $dir  = substr($file->dirname->to_string,length("/var/www/$domain/pages"));
+                my $name = $file->basename;
+                $name =~ s/\.md$/.html/;
+
+                $route_info{"$dir/$name"} = $file->path;
             }
         };
         $mem->set( 'routes', \%route_info );
+        $self->log->info( "Built and stored routes in memcached for $domain" );
     } else {
-        $self->log->info( "Loaded routes from memcached." );
+        $self->log->info( "Loaded routes from memcached for $domain" );
     }
 
-    # Store all end points in a database file for quick recall next time.
-    # url => path HASH with storable or something like that.  Tied hash.
-
-    # Levels of config:
-    # Root      - Owners by me / enable template directories and such
-    # Directory - Owned by the user, control globals for this directory, passes to the template
-    # File      - Owned by the user, passes to the template
-
-    # Store the file in html/ for even faster next time.
-
-    # Serve the file
-
     $self->renderer->paths( [qw( /var/www/themes )] );
-
-    # Create $self->set_template
-    #$self->helper( set_template => sub ($c, $name) {
-    #    $c->stash->{template} = sprintf( "%s/%s", ( $c->config->{template} || 'default' ), $name );
-    #});
-
 
     # Get the router.
     my $router = $self->routes;
@@ -72,15 +62,45 @@ sub startup ($self) {
             my $content = Mojo::File->new( $route_info{$route_path} )->slurp;
             my $md = Markdown::Compiler->new( source => $content );
 
+            # Stash the metadata and rendered markdown so the templates may use it.
             $c->stash->{md} = {
                 %{$md->metadata},
                 body   => $md->result,
                 source => $content,
             };
 
-            $c->stash->{template} = 'default/page';
+            # Set the template to use.
+            $c->stash->{template} = sprintf("%s/%s",
+                $md->metadata->{theme}    || 'default',
+                $md->metadata->{template} || 'page',
+            );
+
+            # Serve and cache if it's an .html file.
+            if ( $r_path =~ /\.html$/ ) {
+                my $html      = $c->render_to_string;
+                my @filepaths = split ( /\//, $r_path );
+                my $filename  = pop @filepaths;
+                $c->render( data => $html, format => 'html' );
+                Mojo::File->new( "/var/www/$domain/html" )->child( @filepaths )->make_path->child($filename)->spurt($html);
+                return;
+            }
         });
     }
+
+    # Status - Show the domain, current routing table.
+    $router->get('/mds.status')->to( cb => sub ($c) {
+        my $status = {
+            domain => $domain,
+            routes => { map { $_ => $route_info{$_}->to_string } keys %route_info },
+        };
+        $c->render( json => $status );
+    });
+
+    # Flush the route -- this needs to happen when the site is rebuilt to purge memcached
+    $router->post('/mds.flush')->to( cb => sub ($c) {
+        $self->log->info( "Flushing routes from memcache for $domain." );
+        $c->render( json => { status => $mem->delete('routes') } );
+    });
 }
 
 1;
