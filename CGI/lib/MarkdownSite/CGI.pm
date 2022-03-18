600809912fb96e79c228e61f9d91bdca7ed3b5cb
Kaitlyn Parkhurst <symkat@symkat.com>
2022-03-18 08:46:35 -0700

    Better URL handling in MDS::CGI

--- CGI/lib/MarkdownSite/CGI.pm
+++ CGI/lib/MarkdownSite/CGI.pm
@@ -12,100 +12,109 @@ sub startup ($self) {
     # Get the router.
     my $router = $self->routes;
 
-    $router->get('*')->to( cb => sub ($c) {
-        my $markdown_file = $self->find_markdown_file_from_path;
+    $router->get('/' )->to( cb => sub ($c) { $self->handle_request($c) });
+    $router->get('/*')->to( cb => sub ($c) { $self->handle_request($c) });
+}
 
-        if ( ! $markdown_file ) {
-            $c->res->body( "MarkdownSite::CGI: File Not Found\n" );
-            $c->res->code( 404 );
-            return;
-            # Serve 404 error.
-        }
+sub handle_request ( $self, $c ) {
 
-        my $content = Mojo::File->new( $markdown_file )->slurp;
-        my $md = Markdown::Compiler->new( source => $content );
+    my $files = $self->resolve_filepaths_from_env;
+    my $markdown_file = $files->{source};
 
-        # Stash the metadata and rendered markdown so the templates may use it.
-        $c->stash->{md} = {
-            %{$md->metadata || {}},
-            body   => $md->result,
-            source => $content,
-        };
-
-        # Set the template to use.
-        my $theme    = $md->metadata ? $md->metadata->{theme}    || 'default' : 'default';
-        my $template = $md->metadata ? $md->metadata->{template} || 'page'    : 'page';
-        $c->stash->{template} = sprintf("%s/%s", $theme, $template );
-
-        my $html_file = $self->find_html_file_from_path;
-
-        # Serve the file to the end user.
-        my $html      = $c->render_to_string;
-        $c->render( data => $html, format => 'html' );
-
-        # Store file so lighttpd serves it next time.
-        my @filepaths = split ( /\//, $html_file );
-        my $filename  = pop @filepaths;
-        Mojo::File->new( $html_file );
-        Mojo::File->new( "/" )->child( @filepaths )->make_path->child($filename)->spurt($html);
+    if ( ! $markdown_file ) {
+        $c->res->body( "MarkdownSite::CGI: File Not Found\n" );
+        $c->res->code( 404 );
         return;
-    });
+        # Serve 404 error.
+    }
+
+    my $content = Mojo::File->new( $markdown_file )->slurp;
+    my $md = Markdown::Compiler->new( source => $content );
+
+    # Stash the metadata and rendered markdown so the templates may use it.
+    $c->stash->{md} = {
+        %{$md->metadata || {}},
+        body   => $md->result,
+        source => $content,
+    };
+
+    # Set the template to use.
+    my $theme    = $md->metadata ? $md->metadata->{theme}    || 'default' : 'default';
+    my $template = $md->metadata ? $md->metadata->{template} || 'page'    : 'page';
+    $c->stash->{template} = sprintf("%s/%s", $theme, $template );
+
+    my $html_file = $files->{target};
+
+    # Serve the file to the end user.
+    my $html      = $c->render_to_string;
+    $c->render( data => $html, format => 'html' );
+
+    # Store file so lighttpd serves it next time.
+    my @filepaths = split ( /\//, $html_file );
+    my $filename  = pop @filepaths;
+    Mojo::File->new( $html_file );
+    Mojo::File->new( "/" )->child( @filepaths )->make_path->child($filename)->spurt($html);
+    return;
+
 }
 
 # Requests for markdown files will respect the following routes:
+#
 # site.com/           -> /var/www/site.com/pages/index.md
 # site.com/about      -> /var/www/site.com/pages/about.md OR /var/www/site.com/pages/about/index.md
 # site.com/about.html -> /var/www/site.com/pages/about.md
 # site.com/about.htm  -> /var/www/site.com/pages/about.md
 #
 #
-# Return path to file, or "" if it doesn't exist..
-
-sub find_markdown_file_from_path {
-    my ( $self ) = @_;
-
+# Return path to file, or "" if it doesn't exist...
+#
+sub resolve_filepaths_from_env ( $self ) {
     my $domain = $ENV{HTTP_HOST};
     my $r_path = $ENV{PATH_INFO};
 
-    # Directory Style
+    print STDERR  "Checking /var/www/$domain/pages/" . $r_path . "/index.md\n";
+    # Case of / /about mapping to /index.md /about/index.md
     if (  -e "/var/www/$domain/pages/" . $r_path . "/index.md" ) {
-        return "/var/www/$domain/pages/" . $r_path . "/index.md";
+        return {
+            source => "/var/www/$domain/pages/" . $r_path . "/index.md",
+            target => "/var/www/$domain/html/" . $r_path . "/index.html",
+        };
     }
 
-    # Switch .html/.htm -> .md
-    $r_path =~ s/\.(html?)/.md/;
+    # Case of about.md -> /about/index.html
+    if ( -e "/var/www/$domain/pages/" . $r_path . ".md" ) {
+        return {
+            source => "/var/www/$domain/pages/" . $r_path . ".md",
+            target => "/var/www/$domain/html/" . $r_path . "/index.html",
+        };
+    }
 
-    # .html Style
-    if (  -e "/var/www/$domain/pages/" . $r_path ) {
-        if ( ! -d "/var/www/$domain/pages/" . $r_path ) {
-            return "/var/www/$domain/pages/" . $r_path;
+    # Case of about.md -> about.html
+    if ( $r_path =~ /\.html$/ ) {
+        (my $base = $r_path) =~ s/\.html$//;
+        if ( -e "/var/www/$domain/pages/$base.md" ) {
+            return {
+                source => "/var/www/$domain/pages/$base.md",
+                target => "/var/www/$domain/html/$base.html",
+            };
         }
     }
 
-    return "";
-}
-
-sub find_html_file_from_path {
-    my ( $self ) = @_;
-
-    my $domain = $ENV{HTTP_HOST};
-    my $r_path = $ENV{PATH_INFO};
-
-    # Directory Style
-    if (  -e "/var/www/$domain/pages/" . $r_path . "/index.md" ) {
-        return "/var/www/$domain/html/" . $r_path . "/index.html";
+    # Case of about.md -> about.htm
+    if ( $r_path =~ /\.htm$/ ) {
+        (my $base = $r_path) =~ s/\.htm$//;
+        if ( -e "/var/www/$domain/pages/$base.md" ) {
+            return {
+                source => "/var/www/$domain/pages/$base.md",
+                target => "/var/www/$domain/html/$base.htm",
+            };
+        }
     }
 
-    # Switch .html/.htm -> .md
-    my $md_path = $r_path;
-    $md_path =~ s/\.(html?)/.md/;
-
-    # .html Style
-    if (  -e "/var/www/$domain/pages/" . $md_path ) {
-        return "/var/www/$domain/html/" . $r_path;
-    }
-
-    return "";
+    return {
+        source => "",
+        target => "",
+    };
 }
 
 1;
